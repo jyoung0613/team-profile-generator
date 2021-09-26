96fac62ba5e394874ed56c2a137fcfa93fb8cc85
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-09-26 16:13:40 -0600

    added a couple npm packages to the index file

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -12,4 +12,37 @@ describe("Employee", () => {
         })
     });
 
+    describe("Name", () => {
+        it("Should set the Name via the constructor function", () => {
+            const input = 'John Young';
+            const expected = {"name": "John Young", "id": 1, "email": "name@name.com"};
+
+            const result = new Employee(input, 1, "name@name.com");
+
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe("Employee ID", () => {
+        it("Should set the Employee ID via the constructor function", () => {
+            const input = 100;
+            const expected = {"name": "First Last", "id": 100, "email": "name@name.com"};
+
+            const result = new Employee("First Last", input, "name@name.com");
+
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe("Email", () => {
+        it("Should set the Email via the constructor function", () => {
+            const input = "jwyoung0613@gmail.com";
+            const expected = {"name": "First Last", "id": 1, "email": "jwyoung0613@gmail.com"};
+
+            const result = new Employee("First Last", 1, input);
+
+            expect(result).toEqual(expected);
+        });
+    });
+
 });
